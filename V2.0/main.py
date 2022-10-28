bda6573f8c47efaf8bd68c74846e2e753dd0e990
Real-4339 <qvadym@stuba.sk>
2022-10-28 15:34:37 +0200

    classes update

--- V2.0/main.py
+++ V2.0/main.py
@@ -6,13 +6,17 @@ class Map:
     def __init__(self) -> None:
         self.width = 200 # width in km
         self.height = 200 # height in km
+        self.cities = [] # list of cities
     
-    def distance(self, x1, y1, x2, y2):
+    def distance(self, x1:int, y1:int, x2:int, y2:int) -> float:
         return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    
+    def add_cities(self, city1, city2, distance):
+        self.cities.append((city1, city2, distance))
 
 
 class Node:
-    def __init__(self):
+    def __init__(self) -> None:
         self.x = random.randrange(0, 200) # x in km
         self.y = random.randrange(0, 200) # y in km
 
