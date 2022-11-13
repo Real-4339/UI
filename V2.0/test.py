44e53f102af6fd313e80f0f53e43b51c20c4b9cf
Real-4339 <qvadym@stuba.sk>
2022-11-13 23:51:50 +0100

    added tabu

--- V2.0/test.py
+++ V2.0/test.py
@@ -1,13 +1,15 @@
 import webbrowser
 import gen_alg
-
+import tabu_alg
 
 if __name__ == '__main__':
     
-    gen_alg.genetics_algorithm()  # type: ignore
+    gen_alg.genetics_algorithm()
+    #tabu_alg.tabu_algorithm()
     
     # try: 
     #     gen_alg.genetics_algorithm()
     # except Exception as e:
     #     print(e)
+    #     #print(gen_alg.genetics_algorithm.__doc__)
     #     #webbrowser.open('https://stackoverflow.com/search?q=' + str(e), new=2, autoraise=True)
