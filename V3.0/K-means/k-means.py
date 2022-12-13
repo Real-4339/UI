4089281991e97c0d94e6c65b877bc8e9fb289076
Vadym Tilihuzov <71652431+Real-4339@users.noreply.github.com>
2022-12-13 13:21:17 +0100

    Update k-means.py

--- V3.0/K-means/k-means.py
+++ V3.0/K-means/k-means.py
@@ -7,24 +7,27 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
-def generation(n: int, points_x, points_y):
-    for i in range(n):
-        if len(points_x) < n:
-            new_x = random.choice(list(points_x))
-            if abs(new_x) >= x_:
-                sign_x = 1 if new_x > 0 else -sign_x
-                x_offset = np.random.randint(0, 100, 1)[0] * sign_x
-            else:
-                x_offset = np.random.randint(0, 100, 1)[0]
-                points_x.add(new_x + x_offset)
-        if len(points_y) < n:
-            new_y = random.choice(list(points_y))
-            if abs(new_y) > y_:
-                sign_y = 1 if new_y > 0 else -1
-                y_offset = np.random.randint(0, 25, 1)[0] * sign_y
-            else:
-                y_offset = np.random.randint(0, 100, 1)[0]
-            points_y.add(new_y + y_offset)
+class Point:
+    def __init__(self, x, y) -> None:
+        self.x = x
+        self.y = y
+    
+    def __hash__(self) -> int:
+        return hash((self.x, self.y))
+    
+    def __eq__(self, o: object) -> bool:
+        if not isinstance(o, Point):
+            return NotImplemented
+        return self.x == o.x and self.y == o.y
+    
+    def __repr__(self) -> str:
+        return f"Point({self.x}, {self.y})"
+
+def distance(p1: Point, x1, y1) -> float:
+    """
+    Calculate the distance between two points
+    """
+    return np.sqrt((p1.x - x1)**2 + (p1.y - y1)**2)
 
 def main(k:int, n:int):
     """
@@ -49,48 +52,59 @@ def main(k:int, n:int):
     x = np.where(np.random.choice([True, False], 20, p=[cube/20, 1-cube/20]), -x, x)
     y = np.where(np.random.choice([True, False], 20, p=[cube/20, 1-cube/20]), -y, y)
     plt.scatter(x, y, c='black', s=7)
-    x = np.array(list(x))
-    y = np.array(list(y))
     # Generating n points
-    points_x:set = set(x)
-    points_y:set = set(y)
-    generation(n, points_x, points_y)
-    #if len(points_x) < n:
-    # add the points to 20 points
-    x = np.concatenate((x, np.array(list(points_x))))
-    y = np.concatenate((y, np.array(list(points_y))))
-    
-    plt.scatter(x, y, c='red', s=7)
-    return
-    # Generating k clusters
-    centroids_x = np.random.randint(0, 100, k)
-    centroids_y = np.random.randint(0, 100, k)
-    centroids = np.array(list(zip(centroids_x, centroids_y))).reshape(len(centroids_x), 2)
-    plt.scatter(centroids_x, centroids_y, c='blue', s=7)
-    # Choose k random points as the initial centroids
-    old_centroids = np.zeros(centroids.shape)
-    clusters = np.zeros(len(points))
-    error = np.linalg.norm(centroids - old_centroids)
-    # Assign each data point to the closest centroid
-    while error != 0:
-        for i in range(len(points)):
-            distances = np.linalg.norm(points[i] - centroids, axis=1)
-            cluster = np.argmin(distances)
-            clusters[i] = cluster
-        # Update the centroid of each cluster
-        old_centroids = centroids
-        for i in range(k):
-            points = [points[j] for j in range(len(points)) if clusters[j] == i]
-            centroids[i] = np.mean(points, axis=0)
-        error = np.linalg.norm(centroids - old_centroids)
-    colors = ['r', 'g', 'b', 'y', 'c', 'm']
-    fig, ax = plt.subplots()
-    for i in range(k):
-        points = np.array([points[j] for j in range(len(points)) if clusters[j] == i])
-        ax.scatter(points[:, 0], points[:, 1], s=7, c=colors[i])
-    ax.scatter(centroids[:, 0], centroids[:, 1], marker='*', s=200, c='#050505')
-        
+    points: set = set(Point(x, y) for x, y in zip(x, y))
+    for i in range(n):
+        new_x = random.choice(list(points))
+        if abs(new_x.x) >= x_:
+            sign_x = 1 if new_x.x > 0 else -sign_x
+            x_offset = np.random.randint(0, 100, 1)[0] * sign_x
+        else:
+            x_offset = np.random.randint(0, 100, 1)[0]
+        new_y = random.choice(list(points))
+        if abs(new_y.y) > y_:
+            sign_y = 1 if new_y.y > 0 else -1
+            y_offset = np.random.randint(0, 25, 1)[0] * sign_y
+        else:
+            y_offset = np.random.randint(0, 100, 1)[0]
+        points.add(Point(new_x.x + x_offset, new_y.y + y_offset))
+    if len(points) < n:
+        for i in range(n - len(points)):
+            new_x = random.choice(list(points))
+            if abs(new_x.x) >= x_:
+                sign_x = 1 if new_x > 0 else -1
+                x_offset = np.random.randint(0, 100, 1)[0] * sign_x
+            else:
+                x_offset = np.random.randint(0, 100, 1)[0]
+            new_y = random.choice(list(points))
+            if abs(new_y.y) > y_:
+                sign_y = 1 if new_y > 0 else -1
+                y_offset = np.random.randint(0, 25, 1)[0] * sign_y
+            else:
+                y_offset = np.random.randint(0, 100, 1)[0]
+            points.add(Point(new_x.x + x_offset, new_y.y + y_offset))
 
+    points = np.array(list(points))
+    # Choose k random points as the initial centroids
+    clusters = np.random.choice(points, k, replace=False)
+    # Make centroid array
+    centroids = np.array([np.array([c.x, c.y]) for c in clusters])
+    # Assign each data point to the closest centroid
+    clusters = np.array([np.argmin([distance(p, c[0], c[1]) for c in centroids]) for p in points])
+    # Update the centroid of each cluster
+    centroids = [np.mean(points[clusters == i], axis=0) for i in range(k)]
+    # Repeat the above steps until the centroids do not change
+    while True:
+        clusters = np.array([np.argmin([distance(p, c[0], c[1]) for c in centroids]) for p in points])
+        new_centroids = [np.mean(points[clusters == i], axis=0) for i in range(k)]
+        if np.all(centroids == new_centroids):
+            break
+        centroids = new_centroids
+    # Plot the data points and the final centroids
+    plt.scatter(points[:, 0], points[:, 1], c=clusters, s=7, cmap='viridis')
+    plt.scatter(centroids[:, 0], centroids[:, 1], c='red', s=200, alpha=0.5)
+    plt.show()
+        
 
 if __name__ == "__main__":
     k, n = input("Enter the number of clusters and number of data points: ").split()
